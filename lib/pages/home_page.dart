29d4c797629d5b10bf0da892c40ebf8088cbf3ef
Jose Candia <josecandiap@gmail.com>
2022-02-16 21:29:59 -0300

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigatorBar.dart';
+
+
+class HomePage extends StatelessWidget {
+  
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Historial'),
+        actions: [
+          IconButton(
+            icon: const Icon( Icons.delete_forever ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+     ),
+     bottomNavigationBar: const CustomNavigationBar(),
+   );
+  }
+}
---
+++ lib/widgets/custom_navigatorBar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomNavigationBar extends StatelessWidget {
+  const CustomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon( Icons.map ),
+          label: 'Mapa'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.compass_calibration ),
+          label: 'Direcciones'
+        ),
+      ],
+    );
+  }
+}
